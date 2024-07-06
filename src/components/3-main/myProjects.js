e363e6edf84d600f64f12da3a5482d1bf7266704
nadagamall <nadagamal087@gmail.com>
2024-07-06 08:30:29 -0700

    add images

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,11 +1,11 @@
 export const myProject = [
     {projectTitle:"all-Movie", ctegory:"all-movie", imgPath:"./public/pic2.jpg"},
     {projectTitle:"drama",ctegory:"drama",imgPath:"./public/Teen_Wolf-_The_Movie_poster1.jpg"},
-    {projectTitle:"comedy",ctegory:"comedy",imgPath:"./public/download.jfif"},
+    {projectTitle:"comedy",ctegory:"comedy",imgPath:"./public/moms-night-out-the-new-family-comedy-from-sony-affirm-and-provident-films-poster-PMB8P8.jpg"},
     {projectTitle:"children",ctegory:"children",imgPath:"./public/jfi.jpeg"},
     {projectTitle:"drama",ctegory:"drama",imgPath:"./public/65_film_teaser_poster.jpg"},
-    {projectTitle:"comedy",ctegory:"comedy",imgPath:"./public/images (2)com.jfif"},
-    {projectTitle:"comedy",ctegory:"comedy",imgPath:"./public/images (2)0.jfif"},
+    {projectTitle:"comedy",ctegory:"comedy",imgPath:"./public/luca_poster.jpg"},
+    {projectTitle:"comedy",ctegory:"comedy",imgPath:"./public/Kingofcomedy.jpg"},
     {projectTitle:"children",ctegory:"children",imgPath:"./public/jjuu.jpg"},
 
 ]; 
