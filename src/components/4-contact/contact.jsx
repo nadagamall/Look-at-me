2914dc79e0dc3deb94332e10d2e66fa7f2efd7dd
nadagamall <nadagamal087@gmail.com>
2024-07-07 13:06:16 -0700

    edit

--- src/components/4-contact/contact.jsx
+++ src/components/4-contact/contact.jsx
@@ -2,12 +2,18 @@ import './contact.css';
 import Lottie from "lottie-react";
 import doneAnimation from "../../../public/animation/Animation - 1718304422137.json";
 import contactusAnimation from "../../../public/animation/Animationcontactus-1718305819752.json";
+import { useForm, ValidationError } from '@formspree/react';
+
+
+
 const Contact = () => {
+    const [state, handleSubmit] = useForm("mjkbvzky");
+   
+
+    
     return (
 
         <section className="contact-us">
-
-
             <h1 className='title'>
                 <span className='icon-envelop'>         </span>
                 Contact Us
@@ -15,27 +21,46 @@ const Contact = () => {
             <p className='sub-title'>We’re here to help!
                 Stream without limits! If you have a question or a comment, our team is one click away.</p>
 
-            <div style={{ justifyContent: "space-between" }} className="flex">
-                <form className=''>
+            <div style={{justifyContent:'space-between'}}className="flex">
+                <form onSubmit={handleSubmit} className=''>
                     <div className='flex'>
                         <label htmlFor="email">Email Address</label>
-                        <input required type="email" name='' id='email' />
+                        <input autoComplete='off' required type="email" name='email' id='email' />
+
+                        <ValidationError
+                            prefix="Email"
+                            field="email"
+                            errors={state.errors}
+                        />
                     </div>
 
                     <div className='flex' style={{ marginTop: "24px" }}>
                         <label htmlFor="message">Your message</label>
-                        <textarea required name='' id='message'></textarea>
+                        <textarea required name='message' id='message'></textarea>
+                        <ValidationError
+                            prefix="Message"
+                            field="message"
+                            errors={state.errors}
+                        />
+
                     </div>
 
-                    <button className='submit'>Submit</button>
+                    <button type="submit" disabled={state.submitting} className='submit'>
+                    {state.submitting ? "Submitting ..." : "Submit"}
+                    </button>
+                       
 
-                    <p className='flex' style={{ fontSize: "18px", marginTop: "1.7rem" }}>
+                   {state.succeeded && (
+                        <p className='flex' style={{ fontSize: "18px", marginTop: "1.7rem" }}>
+                      
                         <Lottie
                             loop={false}
-                            style={{ height: 37 }}
+                            style={{ height:"67px" }}
                             animationData={doneAnimation} />
                         Your message has been sent successfuly
                     </p>
+                )}
+                    
                 </form>
 
                 <div className='animation'>
