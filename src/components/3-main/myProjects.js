381072b718022d4431de5e7149824e460a47de3f
Your Name <nadagamal087@gmail.com>
2024-10-04 19:18:01 +0300

    تحديث مسارات الصور

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,15 +1,4 @@
 export const myProject = [
-<<<<<<< HEAD
-    {projectTitle:"all-Movie", ctegory:"all-movie", imgPath:"/pic2.jpg"},
-    {projectTitle:"drama",ctegory:"drama",imgPath:"/Teen_Wolf-_The_Movie_poster1.jpg"},
-    {projectTitle:"comedy",ctegory:"comedy",imgPath:"/moms-night-out-the-new-family-comedy-from-sony-affirm-and-provident-films-poster-PMB8P8.jpg"},
-    {projectTitle:"children",ctegory:"children",imgPath:"/jfi.jpeg"},
-    {projectTitle:"drama",ctegory:"drama",imgPath:"/65_film_teaser_poster.jpg"},
-    {projectTitle:"comedy",ctegory:"comedy",imgPath:"/luca_poster.jpg"},
-    {projectTitle:"comedy",ctegory:"comedy",imgPath:"/Kingofcomedy.jpg"},
-    {projectTitle:"children",ctegory:"children",imgPath:"/jjuu.jpg"},
-];
-=======
     {projectTitle:"all-Movie", ctegory:"all-movie", imgPath:"/public/pic2.jpg"},
     {projectTitle:"drama",ctegory:"drama",imgPath:"/public/Teen_Wolf-_The_Movie_poster1.jpg"},
     {projectTitle:"comedy",ctegory:"comedy",imgPath:"/public/moms-night-out-the-new-family-comedy-from-sony-affirm-and-provident-films-poster-PMB8P8.jpg"},
@@ -20,5 +9,5 @@ export const myProject = [
     {projectTitle:"children",ctegory:"children",imgPath:"/public/jjuu.jpg"},
 
 ]; 
->>>>>>> 3faca832e37db1ffcdcc3923c998a9e2d741c440
+
 export const myProject2 =[...myProject];
